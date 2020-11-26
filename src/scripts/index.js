fecbfbd40b942af674437d87761719103be81961
makanab <makanabrian1@gmail.com>
2020-11-26 12:29:22 +0300

    clear

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,9 +6,12 @@ const baseUrl = "https://st-johnsuni-api.herokuapp.com/api/";
 const searchq  = document.querySelectorAll('input[type=checkbox]');
 const filter = document.querySelector('#filter-btn');
 const qcode = document.querySelector('#qcode').value;
-const qtitle= document.querySelector('#qtitle').value;
-const qname= document.querySelector('#qname').value;
+const qtitle= document.querySelector('#qtitle').value ;
+const qname= document.querySelector('#qname').value ;
 const qlevel = document.querySelector('#qlevel').value;
+const card = document.querySelector('.card');
+
+
 
 
 
@@ -18,19 +21,12 @@ filter.addEventListener('click',()=>{
         if(list.checked === true){
             filterBy(list.value);
 
-        }
-        
-       
+        }         
 
 
       }
 
-    
-   
 
-
-    
-      
     });
 
 
@@ -42,32 +38,93 @@ filter.addEventListener('click',()=>{
   function filterBy(v){
     
   if(v === 'qcode'){
-    console.log(qcode);
   
-    filterByCode(qcode);
+    filterByCode();
+    
   }
   else if(v === 'qtitle'){
-    filterByTitle(qtitle);
+    filterByTitle();
     }
 
  else if(v === 'qlevel'){
-     filterByLevel(qlevel);
+     filterByLevel();
         }
 
 else if(v === 'qname'){
-        filterByInstructor(qname);
+        filterByInstructor();
             
         }
     }
 
+    function ui(m){
 
-    function filterByCode(v){
-        fetch(baseUrl+`by_course_code/${v}`)
+       card.innerHTML= `
+       <h2 class="card-title">${m.title}</h2>
+       <div class="card-body">
+       <ul>
+        <li>Course code: ${m.course_code} </li>
+        <li>Level: ${m.course_level}</li>
+        <li>Instructor:${m.instructor} </li>
+       </ul>
+       </div>     
+        
+        `;
+
+
+    }
+
+    function filterByCode(){
+        fetch(baseUrl+`by_course_code/${qcode}`)
         .then(m=>{
             return m.json();
         })
         .then(m=>{
-            console.log(m);
+            m.forEach(m=>{ 
+                ui(m);
+               
+            });
         });
 
     }
+
+
+    function filterByTitle(){
+        //console.log('qqqq',v);
+      fetch(baseUrl+`by_title/${qtitle}`)
+      .then(m=>{
+          return m.json();
+      })
+      .then(m=>{
+        m.forEach(m=>{ 
+            ui(m);
+           
+        });
+      });
+
+  }
+
+  function filterByInstructor(){
+    fetch(baseUrl+`by_insructor/${qname}`)
+    .then(m=>{
+        return m.json();
+    })
+    .then(m=>{
+        ui(m);
+    });
+
+}
+
+
+function filterByLevel(v){
+  fetch(baseUrl+`by_level/${qlevel}`)
+  .then(m=>{
+      return m.json();
+  })
+  .then(m=>{
+      ui(m);
+  });
+
+}
+
+
+
